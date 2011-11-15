2da545a89565f98946abe5e723ced1f5270fa2f6
James Dressel <ubergeek@jamesdressel.com>
2011-11-15 14:01:01 -0500

    DOC: Add javaDoc

--- src/InstrumentedList.java
+++ src/InstrumentedList.java
@@ -19,30 +19,50 @@ public class InstrumentedList<E> extends ForwardingList<E> {
 		addCount++;
 		return super.add(e);
 	}
-	
+	/**
+	 * Appends all of the elements in the specified collection to the end of this list, in the order that they are returned by the specified collection's iterator (optional operation).
+	 * @param c collection to add. 
+	 */
 	@Override public boolean addAll(Collection <? extends E> c){
 		addCount += c.size();
 		return super.addAll(c); 
 	}
+	/**
+	 * Inserts the specified element at the specified position in this list (optional operation).
+	 * @param index Position to insert
+	 * @param element element to insert
+	 */
 	@Override
 	public void add(int index, E element) {
 		addCount++;
 		super.add(index, element);
 	}
+	/**
+	 * Inserts all of the elements in the specified collection into this list at the specified position (optional operation).
+	 * @param index position to add in.
+	 * @param c collection to add
+	 */
 	@Override
 	public boolean addAll(int index, Collection<? extends E> c) {
 		addCount += c.size();
 		return super.addAll(index, c);
 	}
-	
+	/**
+	 * 
+	 * @return the number of elements that have been added. 
+	 */
 	public int getAddCount(){
 		return addCount;
 	}
-	
+	/**
+	 * @param o Object to check equality. 
+	 * @return true if objects are equal, false otherwise. 
+	 */
 	@Override
 	public boolean equals(Object o){
 		if(o instanceof InstrumentedList){
 			if(super.equals(o)){
+				@SuppressWarnings("rawtypes")
 				InstrumentedList e = (InstrumentedList)o;
 				if(this.addCount==e.addCount){
 					return true;
@@ -59,7 +79,9 @@ public class InstrumentedList<E> extends ForwardingList<E> {
 		
 
 	}
-	
+	/**
+	 * @return a hash code value
+	 */
 	@Override
 	public int hashCode(){
 		int result = super.hashCode();
